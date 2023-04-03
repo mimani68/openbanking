a18e2dc8888a7e4a4ce1549b2a590407c348da8a
mahdi <imani.mahdi@gmail.com>
2023-04-03 23:09:44 +0330

    wip: update echo server

--- cmd/serve.go
+++ cmd/serve.go
@@ -6,14 +6,10 @@ import (
 	"os/signal"
 	"syscall"
 
-	"github.com/aws/aws-sdk-go/service/account"
-	"github.com/cloudflare/cfssl/transport/roots/system"
-	"github.com/labstack/echo"
 	"github.com/mimani68/fintech-core/config"
+	"github.com/mimani68/fintech-core/data/datasource"
+	"github.com/mimani68/fintech-core/transport/http/echo"
 	"github.com/urfave/cli/v2"
-	"go.uber.org/zap"
-	"go.uber.org/zap/zapcore"
-	"gorm.io/driver/mysql"
 )
 
 var serveCMD = &cli.Command{
@@ -26,30 +22,9 @@ var serveCMD = &cli.Command{
 func serve(c *cli.Context) error {
 	cfg := config.NewConfig()
 
-	f, err := os.OpenFile("logs/app.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
-	if err != nil {
-		fmt.Println(err.Error())
-		return err
-	}
+	db := datasource.NewSqliteDataSource("sample.db")
 
-	logger := zap.New(f, zapcore.ErrorLevel)
-
-	mysqlRepo, err := mysql.New(cfg.Mysql, logger)
-	if err != nil {
-		fmt.Println(err.Error())
-		return err
-	}
-
-	accSrv := account.New(cfg.Account, mysqlRepo, logger)
-	systemSrv := system.New(cfg.Account, mysqlRepo, logger)
-	invitationSrv := invitation.New(cfg.Account, mysqlRepo, logger)
-
-	restServer := echo.New(logger, accSrv, systemSrv, invitationSrv)
-	go func() {
-		if err := restServer.Start(cfg.App.Address); err != nil {
-			logger.Error(fmt.Sprintf("error happen while serving: %v", err))
-		}
-	}()
+	restServer := echo.NewEchoApp(db, cfg)
 
 	signalChan := make(chan os.Signal, 1)
 	signal.Notify(signalChan, os.Interrupt, syscall.SIGTERM)
