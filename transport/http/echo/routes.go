1929cfdd71789071f27f55b1bcf6f00db80c8288
mahdi <imani.mahdi@gmail.com>
2023-04-03 23:03:40 +0330

    feat: handlers of echo

--- transport/http/echo/routes.go
+++ transport/http/echo/routes.go
@@ -2,9 +2,10 @@ package echo
 
 import (
 	"github.com/labstack/echo/v4"
+	"github.com/mimani68/fintech-core/transport/http/echo/handlers"
 )
 
-func (h *Handler) RegisterRouters(urlPrefix *echo.Group) {
+func (h *handlers.Handler) RegisterRouters(urlPrefix *echo.Group) {
 
 	forms := urlPrefix.Group("/forms")
 	forms.GET("", h.ListOfForms)
